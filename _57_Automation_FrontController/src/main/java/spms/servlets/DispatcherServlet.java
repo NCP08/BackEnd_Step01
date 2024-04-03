2c85fa8c5523b2a73b65261a3e0ebd2220c50778
NCP08 <navercloudplatform08.gmail.com>
2024-04-03 11:21:05 +0900

    자동화 작업 진행중...
    ServletRequestDataBinder 클래스의
    static 메서드 구현중...

--- _57_Automation_FrontController/src/main/java/spms/servlets/DispatcherServlet.java
+++ _57_Automation_FrontController/src/main/java/spms/servlets/DispatcherServlet.java
@@ -11,18 +11,33 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import spms.bind.DataBinding;
 import spms.controls.Controller;
-import spms.controls.LogInController;
-import spms.controls.LogOutController;
-import spms.controls.MemberAddController;
-import spms.controls.MemberDeleteController;
-import spms.controls.MemberListController;
-import spms.controls.MemberUpdateController;
-import spms.vo.Member;
 
 @SuppressWarnings("serial")
 @WebServlet("*.do")
 public class DispatcherServlet extends HttpServlet {
+	
+	private void prepareRequestData(HttpServletRequest req, 		// 브라우저가 보내온 Parameter
+									Map<String, Object> model, 		// 자동 생성된 객체를 저장할 model 객체
+									DataBinding dataBinding)		// Controller에서 자동 생성 정보 추출 위한 인터페이스 
+									throws Exception{
+		Object[] dataBinders = dataBinding.getDataBinders();
+		String dataName = null;			// 생성할 객체 이름
+		Class<?> dataType = null;		// 데이터 클래스 타입
+		Object dataObj = null;			// 생성된 객체
+		
+		for(int i=0;i<dataBinders.length;i+=2) {
+			dataName = (String)dataBinders[i];
+			dataType = (Class<?>)dataBinders[i+1];
+			
+			// 데이터 객체 생성
+			dataObj = ServletRequestDataBinder.bind(req, dataType, dataName);
+			
+			// 생성 데이터 model에 저장
+			model.put(dataName, dataObj);
+		}
+	}
 
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -39,6 +54,10 @@ public class DispatcherServlet extends HttpServlet {
 				// 해당 주소와 일치하는 클래스 객체를 꺼내온다.
 				Controller pageController = (Controller) this.getServletContext().getAttribute(servletPath);
 					
+				// DataBinding 인터페이스를 상속받은 pageController인 경우
+				if(pageController instanceof DataBinding) {
+					prepareRequestData(req, model, (DataBinding)pageController);
+				}
 			
 				/* 아래 대신 Reflection을 사용해서 자동화 처리할 예정
 				if ("/member/list.do".equals(servletPath)) {
