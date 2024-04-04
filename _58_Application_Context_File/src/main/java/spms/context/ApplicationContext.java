3120ab1fdc05bd2402e43aa4d5d179d2d5a2cfe5
NCP08 <navercloudplatform08.gmail.com>
2024-04-04 10:21:54 +0900

    _58_Application_Context_File
    WEB-INF/application-context.properties 파일에
    자동으로 생성되어야 할 객체를 기록한다.
    
    이 파일을 AplicationContext 클래스가
    읽어서 자동으로 객체를 생성하고
    의존성 객체를 주입한다.

---
+++ _58_Application_Context_File/src/main/java/spms/context/ApplicationContext.java
@@ -0,0 +1,38 @@
+package spms.context;
+
+import java.io.FileReader;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.Properties;
+
+// application-context.properties 파일을 읽어서 필요한 객체를 준비/저장
+public class ApplicationContext {
+
+	// properties파일이 key=value 구조로 되어 있기때문에 Map을 사용한다.
+	Map<String, Object> objTable = new Hashtable<>();
+	
+	public Object getBean(String key) {
+		return objTable.get(key);
+	}
+	
+	public ApplicationContext(String propertiesPath) throws Exception{
+		Properties props = new Properties();
+		props.load(new FileReader(propertiesPath));
+		
+		prepareObjects(props);
+		injectDependency();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
