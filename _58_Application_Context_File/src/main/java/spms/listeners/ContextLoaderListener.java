f48be0bfac5f359be7ac9b81942a33416d41db13
NCP08 <navercloudplatform08.gmail.com>
2024-04-04 11:25:19 +0900

    _58_Application_Context_File
    Controller객체 생성과
    필요한 객체 자동 주입 완성.
    
    새로운 서비스를 하려면 2가지가 필요하다.
    Dao와 연결/Business로직처리에 필요한 Controller클래스
    그리고, 화면에 정보를 보여줄 jsp 파일
    
    만약 새로운 Controller클래스가 필요하면
    Controller 인터페이스를 상속받고
    appication-context.properties에 등록하면
    나머지는 자동으로 생성되고, MemberDao의 주입도 이루어진다.

--- _58_Application_Context_File/src/main/java/spms/listeners/ContextLoaderListener.java
+++ _58_Application_Context_File/src/main/java/spms/listeners/ContextLoaderListener.java
@@ -1,23 +1,30 @@
 package spms.listeners;
 
-import javax.naming.InitialContext;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
-import javax.sql.DataSource;
 
-import spms.controls.LogInController;
-import spms.controls.LogOutController;
-import spms.controls.MemberAddController;
-import spms.controls.MemberDeleteController;
-import spms.controls.MemberListController;
-import spms.controls.MemberUpdateController;
-import spms.dao.MySqlMemberDao;
+import spms.context.ApplicationContext;
 
 
 @WebListener
 public class ContextLoaderListener implements ServletContextListener{
+	
+	/* ContextLoaderListener는 WebApp이 시작될 때 이벤트 통보를 받는 역할이므로
+	 * 이후에는 사용하지 않는다.
+	 * 그러므로 객체를 별도로 접근하지 않기 때문에
+	 * 객체를 통해 메서드를 호출하지 않고
+	 * 클래스를 통해서 어디에서나 호출할 수 있도록
+	 * static을 사용한다.
+	 * 아래처럼 접근해서 사용하려는 의도
+	 * ApplicationContext appContext = ContextLoaderListener.getApplicationContext();
+	 * */
+	static ApplicationContext applicationContext;
+	
+	public static ApplicationContext getApplicationContext() {
+		return applicationContext;
+	}
 	
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
@@ -26,7 +33,11 @@ public class ContextLoaderListener implements ServletContextListener{
 		try {
 			ServletContext sc = sce.getServletContext();
 			
-
+			String propertiesPath = sc.getRealPath(
+							sc.getInitParameter("contextConfigLocation"));
+			applicationContext = new ApplicationContext(propertiesPath);
+			
+			/*
 			InitialContext initialContext = new InitialContext();
 			DataSource ds = (DataSource)initialContext.lookup(
 						"java:comp/env/jdbc/studydb");
@@ -40,7 +51,7 @@ public class ContextLoaderListener implements ServletContextListener{
 			sc.setAttribute("/member/add.do", new MemberAddController().setMemberDao(memberDao));
 			sc.setAttribute("/member/update.do", new MemberUpdateController().setMemberDao(memberDao));
 			sc.setAttribute("/member/delete.do", new MemberDeleteController().setMemberDao(memberDao));
-			
+			*/
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
